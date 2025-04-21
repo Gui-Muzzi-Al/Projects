56e69f7193e28f767cde8a89d500ed074ae1c36b
Guilherme Alves <muzzigui@icloud.com>
2025-04-21 13:14:51 +0100

    Initial commit

---
+++ my_code/Exercicio_variaveis.py
@@ -0,0 +1,13 @@
+name = "Guilherme"
+last_name = "Alves"
+age = 11
+birth_year = 2025 - age
+of_legal_age = age >= 18
+height_in_meters = 1.85
+
+print("Name:", name)
+print("Last name:", last_name)
+print("Age:", age)
+print("Birth of year:", birth_year)
+print("Are you of legal Age?", of_legal_age)
+print("Height in meters:", height_in_meters)
